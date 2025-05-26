f28a660f42472366501ea92d9ad99f7ff6333419
Ivan Orekhov <makito82@gmail.com>
2025-05-26 17:15:12 +0300

    Update none-ls.lua

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -16,20 +16,14 @@ return {
         'stylua', -- lua formatter
         'eslint_d', -- ts/js linter
         'shfmt', -- Shell formatter
-        'checkmake', -- linter for Makefiles
-        'ruff', -- Python linter and formatter
       },
       automatic_installation = true,
     }
 
     local sources = {
-      diagnostics.checkmake,
       formatting.prettier.with { filetypes = { 'html', 'json', 'yaml', 'markdown' } },
       formatting.stylua,
-      formatting.shfmt.with { args = { '-i', '4' } },
-      formatting.terraform_fmt,
-      require('none-ls.formatting.ruff').with { extra_args = { '--extend-select', 'I' } },
-      require 'none-ls.formatting.ruff_format',
+      formatting.shfmt.with { args = { '-i', '4' } }
     }
 
     local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
